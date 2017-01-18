34bdc7b0a57b7a2d5bfb4dab2282e64181b9384c
izaak-coleman <izaak.coleman11@imperial.ac.uk>
2017-01-18 12:08:08 +0000

    Dont want to loose again!

---
+++ testing_scripts/eval_scripts/algorithm_output_stats/GenomeCoordinatesExtractor.py
@@ -0,0 +1,5 @@
+class GenomeCoordinatesExtractor:
+  """GenomeCoordinatesExtractor takes a fastq file and a list of coordinates,
+  and prints out the DNA sequence from -flanking_dist to +flanking_dist
+  either side of the coordinate from the fastq file"""
+
