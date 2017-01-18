e58557ae0c2e1ac409b2b926439fa1227cbdcfe8
izaak-coleman <izaak.coleman11@imperial.ac.uk>
2017-01-18 15:58:31 +0000

    Prototyping DNA coordinate extraction

--- testing_scripts/eval_scripts/algorithm_output_stats/GenomeCoordinatesExtractor.py
+++ testing_scripts/eval_scripts/algorithm_output_stats/GenomeCoordinatesExtractor.py
@@ -4,9 +4,14 @@ class GenomeCoordinateExtractor:
   """GenomeCoordinatesExtractor takes a fastq file and a list of coordinates,
   and prints out the DNA sequence from -flanking_dist to +flanking_dist
   either side of the coordinate from the fastq file"""
-
-  def __init__(self, fasta_filename, coordinate_filename, flanking_dist):
+  def __init__(self, fasta_filename, coordinate_filename, out_filename,  
+      flanking_dist):
     self.flanking_dist = flanking_dist
+    self.out_filename = out_filename
+    self.fasta_data = {}
+    self.coordinates = []
+    self.coord_seq_pairs = []
+
     self.extractFastaData(fasta_filename)
     self.extractCoordinates(coordinate_filename)
 
@@ -15,9 +20,7 @@ class GenomeCoordinateExtractor:
       The sequence header is used as the key, whilst values are of the
       for (endNPos, sequence), where endNPos is the index of the first leftmost
       non-N character in sequence, and sequence is the fasta string."""
-
     fasta_handle = open(fasta_filename, 'r')
-    self.fasta_data = {}
     header, sequence = "", ""
     for line in fasta_handle:
       if line[0] == ">":      # new sequence, new dict entry
@@ -40,17 +43,37 @@ class GenomeCoordinateExtractor:
         return n
 
   def extractCoordinates(self, coordinate_filename):
-    """ Takes a file with a list of coodinates of form (loc, __, __) where
+    """ Takes a file with a list of coodinates of form (header,loc, __, __) where
       __ can be anything and loc is an integer value representing the
       coordinate of the mutation. Note, format MUST remain comma delim, 
       or modify line slicing. """
-
     coordinate_handle = open(coordinate_filename, 'r')
-    self.coordinates = []
     for line in coordinate_handle:
-      coordinate = line[1:line.find(",")]
-      self.coordinates.append(int(coordinate))
+      header = line[1:line.find(",")]
+      line = line[line.find(",")+1:]
+      location = line[:line.find(",")]
+      self.coordinates.append( (header, int(location)) ) # tupleize
     coordinate_handle.close()
+
+  def extractDNAAroundCoordinates(self):
+    """Function loops through coordinates and extracts the (nEndPos, seq) value
+      associated with the coordinate header (key). The function then
+      extracts the section of DNA around the coordinate that is +/- the 
+      flanking distance of the coordinate and loads data into a list."""
+    for (header, coordinate) in self.coordinates:
+      (n_pos, sequence) = self.fasta_data[header] # retrieve the loc, seq tuple
+      subseq = sequence[coordinate-self.flanking_dist:coordinate]
+      subseq = subseq + sequence[coordinate:coordinate+self.flanking_dist+1]
+      self.coord_seq_pairs.append( (header, coordinate, subseq) )
+
+  def printCoordSeqPairs(self):
+    out_handle = open(self.out_filename, 'w')
+    for (header, coordinate, subseq) in self.coord_seq_pairs:
+      metaData = header + ": " + coordinate
+      out_handle.write(metaData)
+      out_handle.write(subseq)
+      out_handle.write("\n")
+    out_handle.close()
 
   def print_fasta_data(self):
     pprint.pprint(self.fasta_data)
@@ -62,10 +85,11 @@ class GenomeCoordinateExtractor:
     print self.flanking_dist
 
 if __name__ == "__main__":
-  if len(sys.argv) != 4:
+  if len(sys.argv) != 5:
     print "Usage <exe> <fasta_file> <coordinate_file> <flanking_dist>"
 
-  ge = GenomeCoordinateExtractor(sys.argv[1], sys.argv[2], sys.argv[3])
+  ge = GenomeCoordinateExtractor(sys.argv[1], sys.argv[2], sys.argv[3],
+      sys.argv[4]))
   ge.print_fasta_data()
   ge.print_coordinates()
   ge.print_flanking_dist()
