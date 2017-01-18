5514cd892c3242e98209171b74ad3ba22dfa8714
izaak-coleman <izaak.coleman11@imperial.ac.uk>
2017-01-18 12:14:35 +0000

    Rewritten extractFastaData

--- testing_scripts/eval_scripts/algorithm_output_stats/GenomeCoordinatesExtractor.py
+++ testing_scripts/eval_scripts/algorithm_output_stats/GenomeCoordinatesExtractor.py
@@ -3,3 +3,23 @@ class GenomeCoordinatesExtractor:
   and prints out the DNA sequence from -flanking_dist to +flanking_dist
   either side of the coordinate from the fastq file"""
 
+  def __init__(self, fasta_filename, coordinate_filename, flanking_dist):
+    self.flanking_dist = flanking_dist
+    extractFastaData(fasta_filename)
+    extractCoordinate(coordinate_filename)
+
+  def extractFastaData(self, fasta_filename):
+    fasta_handle = open(fasta_filename, 'r')
+    self.fasta_data = {}
+  
+    header, sequence = "", ""
+    for line in fasta_handle:
+      if line[0] == ">":      # start new entry
+        if header != "":
+          self.fasta_data[header] = (nonNStartPos(sequence), sequence)
+        header = line.strip()
+        sequence = ""
+      else:
+        sequence = sequence + line.strip()
+    self.fasta_data[header] = (nonNStartPos(sequence), sequence)
+    fasta_handle.close()
