7ded166887ded273069a58fee50b2125746752bf
izaak-coleman <izaak.coleman11@imperial.ac.uk>
2017-02-01 14:33:06 +0000

    Further refactoring: Switched sorted key to lambda expression

--- testing_scripts/eval_scripts/algorithm_output_stats/queryFastqData/parseResults/sortAndUnique.py
+++ testing_scripts/eval_scripts/algorithm_output_stats/queryFastqData/parseResults/sortAndUnique.py
@@ -1,11 +1,4 @@
 import sys
-from operator import itemgetter, attrgetter, methodcaller
-
-def f7(seq):
-  seen = set()
-  seen_add = seen.add
-  return [x for x in seq if not (x in seen or seen_add(x))]
-
 
 def main():
   tuples = []
@@ -14,10 +7,8 @@ def main():
     tuples = [(int(line[1:line.find(',')]), line[-2:-1]) for line in tuples] 
     tuples = list(set(tuples))
  
-  tuples = sorted(tuples, key=itemgetter(1,0))
+  tuples = sorted(tuples, key=lambda x: (x[1], x[0]))
   for t in tuples:
     print("(%d,%s)" % (t[0], t[1]))
 
 main()
-
-
