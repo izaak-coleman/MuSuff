2c37f3e6c13c246cb7c52c1c753510c7261cfa1b
izaak-coleman <izaak.coleman11@imperial.ac.uk>
2017-02-07 11:17:22 +0000

    Unknown change

--- testing_scripts/eval_scripts/algorithm_output_stats/queryFastqData/parseResults/sortAndUnique.py
+++ testing_scripts/eval_scripts/algorithm_output_stats/queryFastqData/parseResults/sortAndUnique.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python
 import sys
 
 def main():
+  if len(sys.argv) != 2:
+    print "Usage: <exe> <tuple_file>"
+    sys.exit()
+
   tuples = []
   with open(sys.argv[1], 'r') as fsock:
     tuples = [line.strip() for line in fsock]
